aa254427f1ce8f5c646dc872ebd9b70c24d729b9
IsaacM1210 <1210Mhamed@gmail.com>
2025-05-10 10:35:21 -0600

    added axios for backend connection

--- app/axiosInstance.js
+++ app/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: 'http://localhost:5000/',
+    baseURL: 'https://gonanomailer.onrender.com',
     withCredentials: true,
   });
   export default axiosInstance;
